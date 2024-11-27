8f07d72941aa6c4dd131386c58f78f90dfd2f071
Vinicius K² <64318456+vkperitojunior@users.noreply.github.com>
2024-11-27 07:58:47 -0300

    Delete app_spf directory

--- app_spf/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app_spf',
-  webDir: 'www'
-};
-
-export default config;
